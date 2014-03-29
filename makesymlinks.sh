5ee765a2c0123c72396a2ccea6996b24a0dae868
emd <emd@mit.edu>
2014-03-28 18:13:32 -0700

    Added vim-latex setting and user macros files; they too will be symlinked to the appropriate directories using makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -31,3 +31,35 @@ for file in $files; do
     echo "Creating a symlink to $file in \$HOME"
     ln -s $dir/$file ~/.$file
 done
+
+# Also, symlink files for vim-latex settings
+vim_latex=~/.vim/bundle/vim-latex
+
+
+# vim-latex settings
+#
+# $files are contained in dotfiles directory and create vim-latex settings
+# $file_dirs are the subdirectories of $vim_latex in which $files
+# should be placed, e.g. if ${files[0]}=tex.vim and
+# ${file_dirs[0]}=ftplugin then
+#
+#       ln -s $dir/$files[0] $vim_latex/${file_dirs[0]}/${files[0]}
+#
+# will link tex.vim to the appropriate location in vim-latex.
+
+# files and file_dirs must have the *same* size
+files=( "tex.vim" "tex_macros.vim" )
+file_dirs=( "ftplugin" "after/ftplugin" )
+
+for (( i=0; i<${#files[@]}; i++ )); do
+    mkdir -p $vim_latex/${file_dirs[i]}
+    file_path=$vim_latex/${file_dirs[i]}/${files[i]}
+
+    if [ -e $file_path ]; then
+        echo "Moving $file_path to $olddir"
+        mv $file_path $olddir/.
+    fi
+
+    echo "Creating a symlink to ${files[i]} in $vim_latex/${file_dirs[i]}"
+    ln -s $dir/${files[i]} $file_path
+done
